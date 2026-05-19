8c80dc81b22a04222fce52a77bec66236b0b9b15
Prachismita-Panda <prachismitapanda10@gmail.com>
2026-05-20 00:09:22 +0530

    content added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { Toaster } from "react-hot-toast"
+import { Navigate, Route, Routes } from "react-router-dom"
+import LoginLanding from "./pages/LoginLanding"
+import { Layout, Settings } from "lucide-react"
+import Dashboard from "./pages/Dashboard"
+import Employees from "./pages/Employees"
+import Attendance from "./pages/Attendance"
+import Leave from "./pages/Leave"
+import PrintPaySlips from "./pages/PrintPaySlips"
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+        <Toaster/>
+        <Routes>
+          <Route path="/login" element={<LoginLanding/>} />
+          <Route element={<Layout/>}>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/employees" element={<Employees/>}/>
+            <Route path="/attendance" element={<Attendance/>}/>
+            <Route path="/leave" element={<Leave/>}/>
+            <Route path="/paySlips" element={<PrintPaySlipsPaySlips/>}/>
+            <Route path="/settings" element={<Settings/>}/>
+            
+          </Route>
+          <Route path="/print/payslips/:id" element={<PrintPaySlip/>} />
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+    </>
+
   )
 }
 
 export default App
+
