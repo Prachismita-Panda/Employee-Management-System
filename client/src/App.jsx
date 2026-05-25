b007f4fcd3293f76c41d027225705c0023f38f59
Prachismita-Panda <prachismitapanda10@gmail.com>
2026-05-25 17:17:47 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Leave from "./pages/Leave"
 import PaySlips from "./pages/PaySlips"
 import Settings from "./pages/Settings"
 import PrintPaySlips from "./pages/PaySlips"
+import LoginForm from "./components/LoginForm"
 
 const App = () => {
   return (
@@ -16,6 +17,8 @@ const App = () => {
     <Toaster/>
     <Routes>
       <Route path="/login" element={ <LoginLanding/>} />
+      <Route path="/login/admin" element={ <LoginForm role="admin" title="Admin portal" sub-title="Sign in to manage the organization" /> } />
+      <Route path="/login/employee" element={ <LoginForm role="employee" title="employee portal" sub-title="Sign in to access your account"/> } />
         <Route element={<Layout/>}>
             <Route path="/dashboard" element={ <Dashboard/> } />
             <Route path="/employees" element={ <Employees/> } />
--- client/src/pages/LoginLanding.jsx
+++ client/src/pages/LoginLanding.jsx
@@ -1,9 +1,10 @@
+import LoginLeftSide from "../components/LoginLeftSide"
 
 
 const LoginLanding = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <LoginLeftSide/>
     </div>
   )
 }
